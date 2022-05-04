9b46707a2f396d1045ed0503d21b97dc0398bc92
jeong.jinwoo <jeong.jinwoo@joins.com>
2022-05-04 13:17:35 +0900

    Update UserRepositoryTest.java

--- api-server/src/test/java/com/letitgobaby/lifeshare/persistence/UserRepositoryTest.java
+++ api-server/src/test/java/com/letitgobaby/lifeshare/persistence/UserRepositoryTest.java
@@ -1,59 +1,48 @@
 package com.letitgobaby.lifeshare.persistence;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
-import java.util.List;
 
 import com.letitgobaby.lifeshare.domain.model.user.User;
 import com.letitgobaby.lifeshare.domain.model.user.UserRepository;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-// @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class UserRepositoryTest {
 
-  // @Mock
-  private UserRepository userRepository;
+  @Mock
+  UserRepository userRepository;
 
-  @BeforeAll
-  public void setUp() throws Exception {
-    userRepository = mock(UserRepository.class);
-  }
+	@BeforeAll
+	public void setUp() { }
 
   @Test
-  public void regist_user() {
-		String userName = "test";
-		String nickName = "nick";
-		String passwd = "password";
-		String phone = "010-1234-5678";
-		String email = "test@test.com";
-
-    when(userRepository.findByUserName(userName)).thenReturn(new User());
-    
-  }
-  
-	@Test
-	public void user_저장_확인() {
+  public void findByUserName_TEST() {
+		
+		// given
 		String userName = "test";
 		String nickName = "nick";
 		String passwd = "password";
 		String phone = "010-1234-5678";
 		String email = "test@test.com";
 		User newUser = new User().regist(userName, nickName, passwd, phone, email);
-		
-    when(userRepository.save(Mockito.any(User.class))).thenReturn(newUser);
 
-    User savedUser = this.userRepository.save(newUser);
+    when(userRepository.findByUserName(userName)).thenReturn(newUser);
+    
+		// when
+		User user = userRepository.findByUserName(userName);
 
-		assertEquals(savedUser.getUserName(), userName);
-		assertEquals(savedUser.getPhoneNumber(), phone);
-		assertEquals(savedUser.getEmail(), email);
-	}
-
+		// then
+		assertEquals(user.getUserName(), userName);
+		assertEquals(user.getPhoneNumber(), phone);
+		assertEquals(user.getEmail(), email);
+  }
+  
 }
